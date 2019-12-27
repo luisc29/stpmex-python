0871c730952f7e6c0ec9172a4a3c46004480e2fe
Matin Tamizi <matin@cuenca.com>
2019-12-26 19:11:17 -0600

    I guess it should be added here

--- stpmex/__init__.py
+++ stpmex/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ['Client', 'Orden', '__version__']
+__all__ = ['Client', 'Cuenta', 'Orden', '__version__']
 
 from .client import Client
-from .resources import Orden
+from .resources import Cuenta, Orden
 from .version import __version__
