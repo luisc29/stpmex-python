793a363029622352352d9408848d10408ae54e7a
Piyush Ajmani <piyush@cuenca.com>
2020-07-30 12:01:52 -0500

    first commit
    Signed-off-by: Piyush Ajmani <piyush@cuenca.com>

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ SHELL := bash
 PATH := ./venv/bin:${PATH}
 PYTHON = python3.7
 PROJECT = stpmex
-isort = isort -rc -ac $(PROJECT) tests setup.py
+isort = isort $(PROJECT) tests setup.py
 black = black -S -l 79 --target-version py37 $(PROJECT) tests setup.py
 
 
@@ -12,11 +12,14 @@ venv:
 		$(PYTHON) -m venv --prompt $(PROJECT) venv
 		pip install -qU pip
 
-install-test:
-		pip install -q .[test]
+install:
+	pip install -qU -r requirements.txt
+
+install-test: install
+	pip install -qU -r requirements-test.txt
 
 test: clean install-test lint
-		python setup.py test
+		pytest
 
 format:
 		$(isort)
@@ -34,7 +37,7 @@ clean:
 		find . -name '*~' -exec rm -f {} +
 		rm -rf build dist $(PROJECT).egg-info
 
-release: clean
+release: test clean
 		python setup.py sdist bdist_wheel
 		twine upload dist/*
 
--- setup.py
+++ setup.py
@@ -4,24 +4,6 @@ from setuptools import find_packages, setup
 
 version = SourceFileLoader('version', 'stpmex/version.py').load_module()
 
-install_requires = [
-    'pyopenssl==19.1.0',
-    'clabe==1.2.0',
-    'pydantic==1.5.1',
-    'dataclasses>=0.6;python_version<"3.7"',
-    'requests==2.24.0',
-]
-
-test_requires = [
-    'pytest==5.4.3',
-    'pytest-vcr==1.0.2',
-    'pytest-cov==2.10.0',
-    'black==19.10b0',
-    'isort[pipfile]==4.3.21',
-    'flake8==3.8.3',
-    'mypy==0.782',
-]
-
 with open('README.md', 'r') as f:
     long_description = f.read()
 
@@ -38,10 +20,13 @@ setup(
     include_package_data=True,
     package_data=dict(stpmex=['py.typed']),
     python_requires='>=3.6',
-    install_requires=install_requires,
-    setup_requires=['pytest-runner'],
-    tests_require=test_requires,
-    extras_require=dict(test=test_requires),
+    install_requires=[
+        'pyopenssl>=19.1,<19.2',
+        'clabe>=1.2,<1.3',
+        'pydantic>=1.5,<1.6',
+        'dataclasses>=0.6;python_version<"3.7"',
+        'requests>=2.24,<2.25',
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
