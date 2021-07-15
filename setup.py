d2febda3dae836222117dbe849eed2d3eb60ba61
Luis C. Cruz <luiscruz.2908@gmail.com>
2021-07-15 14:47:49 -0500

    chore: required requests version range changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ version = SourceFileLoader('version', 'stpmex/version.py').load_module()
 install_requires = [
     'cryptography>=3.0,<3.5',
     'cuenca-validations>=0.4,<0.8',
-    'requests>=2.24,<2.26',
+    'requests>=2.24',
 ]
 
 
